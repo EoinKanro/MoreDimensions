f4bf074df99576e16f1a22162fedbb1170709355
EoinKanro <ivanros.nh@gmail.com>
2024-10-18 19:14:07 +0400

    Delete useless files

--- src/main/java/io/github/eoinkanro/mc/moredimensions/tools/DimensionError.java
+++
@@ -1,9 +0,0 @@
-package io.github.eoinkanro.mc.moredimensions.tools;
-
-public class DimensionError extends RuntimeException {
-
-    public DimensionError(String message) {
-        super(message);
-    }
-
-}
